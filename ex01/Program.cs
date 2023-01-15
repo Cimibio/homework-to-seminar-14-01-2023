f36099c1503770c29a990bc668f535dc4d2786db
AndreySh <shingaryovan@gmail.com>
2023-01-15 20:59:53 +1200

    сделано первое задание, работает

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine());
+if (num2*num2 == num1)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число НЕ является квадратом второго числа");
+} 
