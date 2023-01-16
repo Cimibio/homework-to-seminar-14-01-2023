0efdc0f2b66597045d42bd7e8825ad1b08596a80
AndreySh <shingaryovan@gmail.com>
2023-01-16 21:05:47 +1200

    добавлено 8е задание по проверке кратности

---
+++ ex08/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите первое число: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число для проверки кратности: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num % num2 == 0 )
+{
+    Console.WriteLine("Число " + num + " кратно числу " + num2);
+}
+else 
+{
+    Console.WriteLine("Число " + num + " НЕ кратно числу " + num2 + ". Остаток: " + num%num2);
+}
