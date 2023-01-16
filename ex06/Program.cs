b0d5b8d7bab41941fccf8a1488406867e4329ea2
AndreySh <shingaryovan@gmail.com>
2023-01-16 18:28:02 +1200

    добавлено 6е задание, показ второй цифры в трехзначном числе

---
+++ ex06/Program.cs
@@ -0,0 +1,4 @@
+﻿// Показать вторую цифру трехзначного числа
+Console.Write("Введите трехзначное число: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая цифра: " + ((N%100 - N%10) / 10));
