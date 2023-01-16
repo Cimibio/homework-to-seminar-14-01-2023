5bf4b71cf4f66128574b54f7f234c121527d96c4
AndreySh <shingaryovan@gmail.com>
2023-01-16 17:51:42 +1200

    добавлено третье задание - проверка четности, работает

---
+++ ex03/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число для проверки четности: ");
+int n = int.Parse(Console.ReadLine());
+if (n%2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else Console.WriteLine("Число нечетное");
