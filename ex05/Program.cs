df11e4870d941f7e5f7f706fe86fc014d5ba4b3e
AndreySh <shingaryovan@gmail.com>
2023-01-16 18:18:45 +1200

    Добавлено 5е задание, работает

---
+++ ex05/Program.cs
@@ -0,0 +1,4 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.Write("Введите треъзначное число: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Последняя цифра: " + N%10);
