9e1b6556a80ae01c8520051adc5b9d93d924a886
AndreySh <shingaryovan@gmail.com>
2023-01-16 19:05:00 +1200

    7е задание, удаление второго числа из трехзначного

---
+++ ex07/Program.cs
@@ -0,0 +1,5 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int N = int.Parse(Console.ReadLine());
+int new_N = (N / 100) * 10 + N%10;
+Console.WriteLine(new_N);
